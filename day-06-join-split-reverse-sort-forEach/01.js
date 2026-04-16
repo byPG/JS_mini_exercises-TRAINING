50a688302862ee2125a579a7737f3091cc5c8fe2
Paulina <pg.gastol@gmail.com>
2026-04-16 22:48:58 +0200

    day 6 - next array's methods

--- day-06-join-split-reverse-sort-forEach/01.js
+++ day-06-join-split-reverse-sort-forEach/01.js
@@ -62,3 +62,107 @@ function joinWords(arr) {
 
 console.log(joinWords(["hello", "world"]));
 console.log(joinWords(["JavaScript", "is", "fun"]));
+
+
+/*
+Zadanie 2
+Napisz funkcję splitSentence(sentence), która zwraca tablicę słów z podanego zdania.
+
+Przykład:
+splitSentence("JavaScript is fun");
+// ["JavaScript", "is", "fun"]
+
+splitSentence("hello world");
+// ["hello", "world"]
+
+Warunki:
+- użyj split
+*/
+function splitSentence(sentence) {
+  return sentence.split(" ");
+}
+
+console.log(splitSentence("JavaScript is fun"));
+console.log(splitSentence("hello world"));
+
+/*
+Zadanie 3
+Napisz funkcję reverseArray(arr), która zwraca nową tablicę
+z elementami w odwróconej kolejności.
+
+Przykład:
+reverseArray([1, 2, 3]);
+// [3, 2, 1]
+
+reverseArray(["a", "b", "c"]);
+// ["c", "b", "a"]
+
+Warunki:
+- użyj reverse
+- nie zmieniaj oryginalnej tablicy
+*/
+function reverseArray(arr) {
+    const newArr = [...arr];
+    return newArr.reverse();
+}
+
+console.log(reverseArray([1, 2, 3]));
+console.log(reverseArray(["a", "b", "c"]));
+
+
+
+/*
+Zadanie 4
+Napisz funkcję sortNumbersAsc(arr), która zwraca nową tablicę
+z liczbami posortowanymi rosnąco.
+
+Przykład:
+sortNumbersAsc([10, 2, 30]);
+// [2, 10, 30]
+
+sortNumbersAsc([5, 1, 8]);
+// [1, 5, 8]
+
+Warunki:
+- użyj sort
+- nie zmieniaj oryginalnej tablicy
+*/
+function sortNumbersAsc(arr) {
+  const newArr = [...arr];
+  return newArr.sort((a, b) => a - b);
+}
+
+console.log(sortNumbersAsc([10, 2, 30]));
+console.log(sortNumbersAsc([5, 1, 8]));
+
+
+
+/*
+Zadanie 5
+Napisz funkcję logWords(arr), która wypisuje każde słowo w konsoli.
+
+Przykład:
+logWords(["dog", "cat", "banana"]);
+
+W konsoli:
+dog
+cat
+banana
+
+Warunki:
+- użyj forEach
+- funkcja nie musi nic zwracać
+*/
+function logWords(arr) {
+  arr.forEach(item => console.log(item));
+}
+
+logWords(["dog", "cat", "banana"]);
+logWords(["JS", "HTML", "CSS"]);
+
+/*PODSUMOWANIE*/
+// join()    -> tablica na string
+// split()   -> string na tablicę
+// reverse() -> odwraca tablicę, mutuje
+// sort()    -> sortuje tablicę, mutuje
+// forEach() -> wykonuje akcję dla każdego elementu
